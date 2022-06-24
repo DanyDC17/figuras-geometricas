cd6be37b07ecef99137b1dd3416ee29d397788c2
DanyDC17 <107901396+DanyDC17@users.noreply.github.com>
2022-06-24 13:07:29 -0500

    Add files via upload

---
+++ turtle_espirales.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 22 10:12:48 2022
+
+@author: Daniel
+"""
+
+from turtle import*
+import random
+title('ESPIRALES')
+#bigcolor('black')
+pencolor('green')
+speed(0)
+x=1
+while x<4000:
+ 
+    forward(5+x)
+    right(3344)
+    x+=1
+mainloop()
